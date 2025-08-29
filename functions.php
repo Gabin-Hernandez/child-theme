e023789667205773b49786cb047b4f50b0ea8299
Gabin-Hernandez <86694856+Gabin-Hernandez@users.noreply.github.com>
2025-08-29 00:23:22 -0600

    child theme

--- functions.php
+++ functions.php
@@ -1,4 +1,13 @@
 <?php
-add_action('wp_enqueue_scripts', function() {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-});
+// Función para encolar los estilos del tema padre y del hijo
+function child_theme_enqueue_styles() {
+    $parent_style = 'storefront-style'; // Nombre del estilo del tema padre
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_styles' );
