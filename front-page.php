b3824546c0648d3ce5252cc406aae6ce47c0e7db
Juan Hernandez <juanhernandez.mg@gmail.com>
2025-10-20 10:01:41 -0600

    Remove thumbnail requirement for visible products in LCD screens query

--- front-page.php
+++ front-page.php
@@ -2121,7 +2121,7 @@ get_header(); ?>
                     while ( $pantallas_query->have_posts() ) : 
                         $pantallas_query->the_post(); 
                         global $product;
-                        if ( $product && $product->is_visible() && has_post_thumbnail() ) {
+                        if ( $product && $product->is_visible() ) {
                             $valid_products[] = array(
                                 'post' => get_post(),
                                 'product' => $product
